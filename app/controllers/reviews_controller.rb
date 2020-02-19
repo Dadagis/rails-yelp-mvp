573a3c03088d2e1ff3bf5ef905961a4cb28bbd4e
David Jungblut <davidobob@laposte.net>
2020-02-19 15:52:57 +0100

    Yelp MVP done

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,36 @@
+class ReviewsController < ApplicationController
+  before_action :set_restaurant, only: [:new, :create]
+
+  def show
+  end
+
+  def index
+    @review = Review.all
+
+  end
+
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @review.restaurant = @restaurant
+    @review.save
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
+end
